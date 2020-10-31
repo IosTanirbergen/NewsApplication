f86f794b7356dad18a9a13be32e36133a8c8213c
SwiftTanirbergen <developer.tanirbergen@mail.ru>
2020-10-31 17:09:28 +0600

    Start NewsApplication

---
+++ NewsApplication/View/ViewData.swift
@@ -0,0 +1,11 @@
+//
+//  ViewData.swift
+//  NewsApplication
+//
+//  Created by Devnull on 10/31/20.
+//  Copyright © 2020 Tanirbergen Kaldibai. All rights reserved.
+//
+
+import Foundation
+
+
---
+++ NewsApplication/ViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApplication
+//
+//  Created by Devnull on 10/29/20.
+//  Copyright © 2020 Tanirbergen Kaldibai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
